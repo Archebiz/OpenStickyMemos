0491312a7ce7e21b638b3ec9bcc53543a08b7f8f
Archebiz <adriansalazar2497@gmail.com>
2026-06-30 23:24:45 -0500

    fix: agregar permissions: contents:write al workflow de release

--- src/desktop/OpenStickyMemos.Desktop/Views/MainWindow.xaml.cs
+++ src/desktop/OpenStickyMemos.Desktop/Views/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
 using OpenStickyMemos.Desktop.Services;
 using OpenStickyMemos.Desktop.ViewModels;
 
@@ -22,5 +23,28 @@ public partial class MainWindow : Window
     {
         if (Activator.CreateInstance(viewType) is UserControl view)
             MainContentArea.Content = view;
+    }
+
+    private void TitleBar_MouseDown(object sender, MouseButtonEventArgs e)
+    {
+        if (e.ChangedButton == MouseButton.Left)
+            DragMove();
+    }
+
+    private void Minimize_Click(object sender, RoutedEventArgs e)
+    {
+        WindowState = WindowState.Minimized;
+    }
+
+    private void Maximize_Click(object sender, RoutedEventArgs e)
+    {
+        WindowState = WindowState == WindowState.Maximized
+            ? WindowState.Normal
+            : WindowState.Maximized;
+    }
+
+    private void Close_Click(object sender, RoutedEventArgs e)
+    {
+        Close();
     }
 }
